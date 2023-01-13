cf2aa341cccedab489a14cb43f35ab6380d40f04
Burak Karagol <34738016+BurakKaragol@users.noreply.github.com>
2023-01-13 17:27:32 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -123,7 +123,7 @@ const form = document.forms['submit-to-google-sheet']
 const msg = document.getElementById("msg")
 
 form.addEventListener('submit', e => {
-    // e.preventDefault() for sending the email at the same time
+    e.preventDefault() //for sending the email at the same time
     fetch(scriptURL, { method: 'POST', body: new FormData(form)})
     .then(response => 
     {
