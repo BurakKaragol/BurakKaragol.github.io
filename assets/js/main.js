2c22edaef964ded789cf9107f227863b0f5a7a3a
Burak Karagol <brk.krgl.34@hotmail.com>
2023-02-06 20:20:17 +0300

    Website remake

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,146 +1,158 @@
-/*===== SHOW MENU =====*/
-const showMenu = (toggleId, navId) => 
-{
-    const toggle = document.getElementById(toggleId),
-    nav = document.getElementById(navId)
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
 
-    if (toggle && nav)
-    {
-        toggle.addEventListener('click', () => 
-        {
-            nav.classList.toggle('show-menu')
-        })
-    }
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
 }
-showMenu('nav-toggle','nav-menu')
 
-/*===== REMOVE MENU MOBILE =====*/
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+/*=============== REMOVE MENU MOBILE ===============*/
 const navLink = document.querySelectorAll('.nav__link')
 
-function linkAction()
-{
+const linkAction = () =>{
     const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
     navMenu.classList.remove('show-menu')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
-/*===== SCROLL SECTIONS ACTIVE LINK =====*/
+/*=============== SWIPER PROJECTS ===============*/
+let swiperProjects = new Swiper(".projects__container", {
+    loop: true,
+    spaceBetween: 24,
+    
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+    },
+    breakpoints: {
+        1200: {
+            slidesPerView: 2,
+            spaceBetween: -56,
+        },
+    },
+});
+
+/*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById('contact-form'),
+    contactName = document.getElementById('contact-name'),
+    contactEmail = document.getElementById('contact-email'),
+    contactMessage = document.getElementById('contact-message'),
+    contactMsg = document.getElementById('contact-msg')
+
+const sendEmail = (e) =>{
+    e.preventDefault()
+    console.log('girdi')
+    if(contactName.value === '' || contactEmail === '' || contactMessage === ''){
+        contactMessage.classList.remove('color-blue')
+        contactMessage.classList.add('color-red')
+
+        contactMessage.textContent = 'Please fill all the input fields 📩'
+    }
+    else{
+        emailjs.sendForm('service_sohc712', 'template_dtve7zq', '#contact-form', '3Xlz6Ok2ZRJTuktB2')
+            .then(() =>{
+                contactMsg.classList.add('color-blue')
+                contactMsg.textContent = 'Message sent ✅'
+                
+                setTimeout(() => {
+                    contactMessage.textContent = ''
+                }, 5000)
+            }, (error) =>{
+                console.error(error)
+                alert('Something went wrong 😢')
+            })
+
+        contactName.value = ''
+        contactEmail.value = ''
+        contactMessage.value = ''
+    }
+}
+contactForm.addEventListener('submit', sendEmail)
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 const sections = document.querySelectorAll('section[id]')
+    
+const scrollActive = () =>{
+  	const scrollY = window.pageYOffset
 
-function scrollActive() 
-{
-    const scrollY = window.pageYOffset
+	sections.forEach(current =>{
+		const sectionHeight = current.offsetHeight,
+			  sectionTop = current.offsetTop - 58,
+			  sectionId = current.getAttribute('id'),
+			  sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
 
-    sections.forEach(current => 
-    {
-        const sectionHeight = current.offsetHeight
-        const sectionTop = current.offsetTop - 50
-        sectionId = current.getAttribute('id')
-
-        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight)
-        {
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
-        }
-        else
-        {            
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
-        }
-    })
+		if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+			sectionsClass.classList.add('active-link')
+		}else{
+			sectionsClass.classList.remove('active-link')
+		}                                                    
+	})
 }
 window.addEventListener('scroll', scrollActive)
 
-/*===== CHANGE BACKGROUND HEADER =====*/ 
-function scrollHeader()
-{
+/*=============== SHOW SCROLL UP ===============*/ 
+const scrollUp = () =>{
+	const scrollUp = document.getElementById('scroll-up')
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+	this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+						: scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+/*=============== DARK LIGHT THEME ===============*/ 
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const IconMoon = 'ri-moon-line'
+const IconSun = 'ri-sun-line'
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+    // Add or remove the dark / icon theme
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(IconMoon)
+    themeButton.classList.toggle(IconSun)
+})
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+const scrollHeader = () =>{
     const header = document.getElementById('header')
-    if (this.scrollY >= 200) header.classList.add('scroll-header');
-    else header.classList.remove('scroll-header');
+    // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+    this.scrollY >= 50 ? header.classList.add('bg-header') 
+                       : header.classList.remove('bg-header')
 }
 window.addEventListener('scroll', scrollHeader)
 
-/*===== SHOW SCROLL TOP =====*/ 
-function scrollTop()
-{
-    const scrollTop = document.getElementById('scroll-top')
-    if (this.scrollY >= 560) scrollTop.classList.add('show-scroll');
-    else scrollTop.classList.remove('show-scroll');
-}
-window.addEventListener('scroll', scrollTop)
-
-/*===== MIXITUP FILTER PORTFOLIO =====*/ 
-var mixer1 = mixitup(".portfolio__container", 
-{
-    selectors: 
-    {
-        target: '.portfolio__content'
-    },
-    animation: 
-    {
-        duration: 300
-    }
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    reset: true
 });
 
-var mixer2 = mixitup(".skills__container", 
-{
-    selectors: 
-    {
-        target: '.skills__content'
-    },
-    animation: 
-    {
-        duration: 300
-    }
-});
-
-/* Link active portfolio */ 
-const linkPortfolio = document.querySelectorAll('.portfolio__item')
-
-function activePortfolio()
-{
-    if (linkPortfolio)
-    {
-        linkPortfolio.forEach(l => l.classList.remove('active-portfolio'))
-        this.classList.add('active-portfolio')
-    }
-}
-linkPortfolio.forEach(l => l.addEventListener('click', activePortfolio))
-
-
-const linkSkills = document.querySelectorAll('.skills__item')
-
-function activeSkill()
-{
-    if (linkSkills)
-    {
-        linkSkills.forEach(l => l.classList.remove('active-skill'))
-        this.classList.add('active-skill')
-    }
-}
-linkSkills.forEach(l => l.addEventListener('click', activeSkill))
-
-const scriptURL = 'https://script.google.com/macros/s/AKfycbzb8MAa8v2CKyP8v53E-CBF_qG-VJkWpbYrDP-FmcQPKr1JbIpYEA2t-L4BhREE6XUO/exec'
-const form = document.forms['submit-to-google-sheet']
-const msg = document.getElementById("msg")
-
-form.addEventListener('submit', e => {
-    e.preventDefault() //for sending the email at the same time
-    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-    .then(response => 
-    {
-        msg.innerHTML = "Message sent successfully!"
-        setTimeout(function()
-        {
-            msg.innerHTML = ""
-        }, 5000)
-        form.reset()
-    })
-    .catch(error => console.error('Error!', error.message))
-})
-
-/*===== GSAP ANIMATION =====*/ 
-gsap.from('.home__img', {opacity: 0, duration: 2, delay: 0.5, x: 60})
-gsap.from('.home__data', {opacity: 0, duration: 2, delay: 0.8, y: 25})
-gsap.from('.home__greeting, home__name, home__profession, .home__button', {opacity: 0, duration: 2, delay: 1, y: 25, ease: 'expo.out', stagger: 0.2})
-gsap.from('.nav__logo, .nav__toggle', {opacity: 0, duration: 2, delay: 1.5, y: 25, ease: 'expo.out', stagger: 0.2})
-gsap.from('.nav__item', {opacity: 0, duration: 2, delay: 1.8, y: 25, ease: 'expo.out', stagger: 0.2})
-gsap.from('.home__social-icon', {opacity: 0, duration: 2, delay: 2.3, y: 25, ease: 'expo.out', stagger: 0.2})
+sr.reveal('.home__data, .projects__container, .footer__container')
+sr.reveal('.home__info div', {delay: 600, origin: 'bottom', interval: 100})
+sr.reveal('.skills__content:nth-child(1), .skills__content:nth-child(3), .contact__content:nth-child(1)', {origin: 'left'})
+sr.reveal('.skills__content:nth-child(2), .skills__content:nth-child(4), .contact__content:nth-child(2)', {origin: 'right'})
+sr.reveal('.qualification__content, .services__card', {interval: 100})
