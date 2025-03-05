8ca56797d06dbfb81b0de5066ba208e474a9caff
Burak Karagol <brk.krgl.34@hotmail.com>
2025-03-05 21:49:58 +0300

    fixing the links

--- links/link_main.js
+++ links/link_main.js
@@ -1,13 +1,13 @@
 
 const links = [
     { name: "My Website", url: "https://burakkaragol.github.io/", icon: "fa-solid fa-rocket" },
-    { name: "LinkedIn", url: "https://www.linkedin.com/in/burakkaragol/", icon: "fa-brands fa-linkedin" },
-    { name: "GitHub", url: "https://github.com/burakkaragol", icon: "fa-brands fa-github" },
-    { name: "Itch.io", url: "https://burakkaragol.itch.io/", icon: "fa-brands fa-itch-io" },
-    { name: "Instagram", url: "https://www.instagram.com/burakkaragol/", icon: "fa-brands fa-instagram" },
-    { name: "Twitch", url: "https://www.twitch.tv/burakkaragol", icon: "fa-brands fa-twitch" },
-    { name: "YouTube", url: "https://www.youtube.com/@burakkaragol", icon: "fa-brands fa-youtube" },
-    { name: "Twitter", url: "https://twitter.com/burakkaragol", icon: "fa-brands fa-x-twitter" }
+    { name: "LinkedIn", url: "https://www.linkedin.com/in/burak-karagol-3451b91b0/", icon: "fa-brands fa-linkedin" },
+    { name: "GitHub", url: "https://github.com/BurakKaragol", icon: "fa-brands fa-github" },
+    { name: "Itch.io", url: "https://mrlulez.itch.io/", icon: "fa-brands fa-itch-io" },
+    { name: "Instagram", url: "https://www.instagram.com/brkkaragol/?hl=tr", icon: "fa-brands fa-instagram" },
+    { name: "Twitch", url: "https://www.twitch.tv/mrlulez", icon: "fa-brands fa-twitch" },
+    { name: "YouTube", url: "https://www.youtube.com/channel/UCMqP-wqi9hXqKfkzfmoqa-w", icon: "fa-brands fa-youtube" },
+    { name: "Twitter", url: "https://twitter.com/buraKaragol", icon: "fa-brands fa-x-twitter" }
 ];
 
 const container = document.getElementById('linksContainer');
